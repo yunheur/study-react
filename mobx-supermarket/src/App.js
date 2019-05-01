145572637f5b4c6576a2892a4edfe91f26018e63
yunheur <yunheur@gmail.com>
2019-05-01 09:29:35 +0900

    write supermarket ui

---
+++ mobx-supermarket/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from './components/Counter';
+import SuperMarket from './components/SuperMarket';
+
+const App = () => {
+  return (
+    <>
+      <Counter />
+      <hr />
+      <SuperMarket />
+    </>
+  );
+};
+
+export default App;
